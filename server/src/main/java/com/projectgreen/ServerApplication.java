1933ddd65f1ad380612b4439c0cebd8d96b3ba8a
oburnett127 <oburnett127@gmail.com>
2022-11-08 19:24:42 -0600

    initial commit for backend project structure creation

---
+++ server/src/main/java/com/projectgreen/ServerApplication.java
@@ -0,0 +1,15 @@
+package com.projectgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.projectgreen")
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+
+        System.out.println("Project Green is running ==============================================================");
+    }
+}
