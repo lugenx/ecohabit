a327a6f28e6fa0b747d2e2c0b5b4ca66bf361f72
Elgun Asgar <elgunasgerov@gmail.com>
2022-12-03 13:03:21 -0500

    fix getLocationDetails array query issue

--- mock-api/methods/getLocationDetails.js
+++ mock-api/methods/getLocationDetails.js
@@ -10,13 +10,26 @@ router.get("/", async (req, res) => {
     res.send({ err: "invalid key" });
   }
 
-  const API_KEY = process.env.API_KEY;
+  const query = req.query;
   const locationId = req.query.location_id;
+  delete query.location_id;
+  query.api_key = process.env.API_KEY;
+
+  let locationIdStr = "";
+  if (Array.isArray(locationId)) {
+    for (let id of locationId) {
+      locationIdStr = locationIdStr + `&location_id[]=${id}`;
+    }
+  } else if (locationId) {
+    locationIdStr = `&location_id=${locationId}`;
+  }
+
+  const str = new URLSearchParams(query).toString() + locationIdStr;
+
+  const url = `http://api.earth911.com/earth911.getLocationDetails?${str}`;
 
   try {
-    const data = await fetchData(
-      `http://api.earth911.com/earth911.getLocationDetails?api_key=${API_KEY}&location_id=${locationId}`
-    );
+    const data = await fetchData(url);
     res.send(data);
   } catch (error) {
     console.log("ERROR: ", error);
