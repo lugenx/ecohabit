52d6b16c047019ab92f34dd94f732897da608511
Winter <90015510+WinterSunset95@users.noreply.github.com>
2022-12-04 08:26:44 +0000

    navbar design

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './NavBar.css'
+import DeleteIcon from '@mui/icons-material/Delete';
+import DriveEtaIcon from '@mui/icons-material/DriveEta';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
 
 const NavBar = () => {
+  const [active, setActive] = React.useState('recycling-habits')
   return (
-    <h1>A PLACEHOLDER FOR UPCOMING NAVBAR COMPONENT</h1>
+    <nav>
+      <h1>EcoHabit</h1>
+      <ul>
+        <li className={`${active == 'recycling-habits' ? 'active' : ''}`} onClick={() => setActive('recycling-habits')}><DeleteIcon /><span>Recycling Habits</span></li>
+        <li className={`${active == 'commuting-habits' ? 'active' : ''}`} onClick={() => setActive('commuting-habits')}><DriveEtaIcon /><span>Commuting Habits</span></li>
+        <li className={`${active == 'eating-habits' ? 'active' : ''}`} onClick={() => setActive('eating-habits')}><RestaurantIcon /><span>Eating Habits</span></li>
+        <li className={`${active == 'energy-habits' ? 'active' : ''}`} onClick={() => setActive('energy-habits')}><FlashOnIcon /><span>Energy Use Habits</span></li>
+      </ul>
+    </nav>
   )
 }
 
