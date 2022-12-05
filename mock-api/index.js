af540539da3e98a70dd986286232f91a57495dd3
Elgun Asgar <elgunasgerov@gmail.com>
2022-12-04 22:27:50 -0500

    add query limit 12 requests per minute per ip address.

--- mock-api/index.js
+++ mock-api/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 
 import { router as getPostalDataMethod } from "./methods/getPostalData.js";
 import { router as searchLocationsMethod } from "./methods/searchLocations.js";
@@ -7,7 +8,14 @@ import { router as getLocationDetailsMethod } from "./methods/getLocationDetails
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const apiRequestLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 12, // each IP limited 12 requests per minute
+});
+
+app.use(apiRequestLimiter);
 app.use(express.json());
+
 app.use("/earth911.getPostalData", getPostalDataMethod);
 app.use("/earth911.searchLocations", searchLocationsMethod);
 app.use("/earth911.getLocationDetails", getLocationDetailsMethod);
