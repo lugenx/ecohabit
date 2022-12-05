1bf9c88be1a535eea773f2e754ee0e211700b251
Winter <90015510+WinterSunset95@users.noreply.github.com>
2022-12-05 04:39:39 +0000

    Improved navbar design

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,21 +1,63 @@
 import React from 'react'
+// Ui components
+import { Link, NavLink } from 'react-router-dom'
+import { LinearProgress } from '@mui/material';
+// Stylesheet
 import './NavBar.css'
+// Icons
 import DeleteIcon from '@mui/icons-material/Delete';
 import DriveEtaIcon from '@mui/icons-material/DriveEta';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
 import FlashOnIcon from '@mui/icons-material/FlashOn';
+import HomeIcon from '@mui/icons-material/Home';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const NavBar = () => {
-  const [active, setActive] = React.useState('recycling-habits')
+  const [loggedIn, setLoggedIn] = React.useState(true)
+  const [points, setPoints] = React.useState(10)
+  const [imgurl, setImgurl] = React.useState('https://avatars.githubusercontent.com/u/90015510?v=4')
+  let activeStyle = {
+    color: '#44bd32',
+    backgroundColor: '#252b36',
+  }
   return (
     <nav>
       <h1>EcoHabit</h1>
-      <ul>
-        <li className={`${active == 'recycling-habits' ? 'active' : ''}`} onClick={() => setActive('recycling-habits')}><DeleteIcon /><span>Recycling Habits</span></li>
-        <li className={`${active == 'commuting-habits' ? 'active' : ''}`} onClick={() => setActive('commuting-habits')}><DriveEtaIcon /><span>Commuting Habits</span></li>
-        <li className={`${active == 'eating-habits' ? 'active' : ''}`} onClick={() => setActive('eating-habits')}><RestaurantIcon /><span>Eating Habits</span></li>
-        <li className={`${active == 'energy-habits' ? 'active' : ''}`} onClick={() => setActive('energy-habits')}><FlashOnIcon /><span>Energy Use Habits</span></li>
-      </ul>
+      <div className='nav-mid'>
+        <div className='nav-progressbar'>
+          <span style={{ paddingRight: '0.175rem' }}>{points}</span>
+          <span style={{ paddingRight: '1rem' }}>Points</span>
+          <LinearProgress
+            variant='buffer'
+            value={points}
+            valueBuffer={100}
+            style={{ height: '1rem', width: '100%', borderRadius: '1rem' }}
+          />
+        </div>
+        <ul className='nav-links'>
+          <NavLink to="/home" style={({ isActive }) => isActive ? activeStyle : undefined } className={`nav-link-item`}><HomeIcon /><span>Home</span></NavLink>
+          <NavLink to="/fave" style={({ isActive }) => isActive ? activeStyle : undefined } className={`nav-link-item`}><FavoriteIcon /><span>Habits</span></NavLink>
+          <NavLink to="/rcyc" style={({ isActive }) => isActive ? activeStyle : undefined } className={`nav-link-item`}><DeleteIcon /><span>Recycle</span></NavLink>
+          <NavLink to="/comm" style={({ isActive }) => isActive ? activeStyle : undefined } className={`nav-link-item`}><DriveEtaIcon /><span>Commuting Habits</span></NavLink>
+          <NavLink to="/eats" style={({ isActive }) => isActive ? activeStyle : undefined } className={`nav-link-item`}><RestaurantIcon /><span>Eating</span></NavLink>
+          <NavLink to="/nrgy" style={({ isActive }) => isActive ? activeStyle : undefined } className={`nav-link-item`}><FlashOnIcon /><span>Energy</span></NavLink>
+        </ul>
+      </div>
+      <div className='nav-end'>
+        {loggedIn ? 
+          <div className='nav-end'>
+            <Link style={{ paddingRight: '2rem', color: 'white' }}><NotificationsIcon style={{ transform: 'scale(1.5)' }}/></Link>
+            <Link to='/user'>
+              <div className='user-btn'>
+                <img src={imgurl} alt="" srcset="" />
+              </div>
+            </Link> 
+          </div>
+          : 
+          <Link to='/login' className="login-btn">Login</Link>
+        }
+      </div>
     </nav>
   )
 }
