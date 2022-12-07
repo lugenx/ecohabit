edc75178caea61384e0c3936c56a9d7b1fc62b81
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-12-06 23:31:47 -0800

    add global theme and a css reset provided by mui

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider, CssBaseline } from "@mui/material/";
+import theme from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
