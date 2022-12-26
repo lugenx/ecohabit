f6e4d4e8ee5cf5959a180392cbb327d0b96776ac
ashwaryy <ashwaryy@gmail.com>
2022-12-26 20:23:02 +0530

    added custom validator for usinque email

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -10,7 +10,22 @@ const userSchema = new Schema(
     },
     email: {
       type: String,
-      required: true,
+      // unique: true,
+      lowercase: true,
+      validate: {
+        validator: async function (email) {
+          const user = await this.constructor.findOne({ email });
+          if (user) {
+            if (this.id === user.id) {
+              return true;
+            }
+            return false;
+          }
+          return true;
+        },
+        message: () => "Email address already registered.",
+      },
+      required: [true, "User email required"],
     },
     postalCode: {
       type: String,
