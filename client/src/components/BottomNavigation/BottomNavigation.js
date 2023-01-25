6216afb906981c1d809e1bd4e2abc644c3926d87
Aleksei Treshchalin <treshchal@gmail.com>
2023-01-25 11:52:45 +0300

    Add BottomNavigation component Add BottomNavigation.js Pull the example from MUI docs Organize imports

---
+++ client/src/components/BottomNavigation/BottomNavigation.js
@@ -0,0 +1,41 @@
+import * as React from "react";
+// Ui components
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+// Icons
+import FolderIcon from "@mui/icons-material/Folder";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+
+export default function LabelBottomNavigation() {
+  const [value, setValue] = React.useState("recents");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
+      <BottomNavigationAction
+        label="Recents"
+        value="recents"
+        icon={<RestoreIcon />}
+      />
+      <BottomNavigationAction
+        label="Favorites"
+        value="favorites"
+        icon={<FavoriteIcon />}
+      />
+      <BottomNavigationAction
+        label="Nearby"
+        value="nearby"
+        icon={<LocationOnIcon />}
+      />
+      <BottomNavigationAction
+        label="Folder"
+        value="folder"
+        icon={<FolderIcon />}
+      />
+    </BottomNavigation>
+  );
+}
