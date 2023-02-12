aaa6525457d14921aa4ae44a1af7ccc6cabc3e4b
Rob Lehmann <lehmannrobb@gmail.com>
2023-02-12 21:42:13 +0300

    Added getAll to habit controller/routes and dispayed list of users habits on homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,5 +1,45 @@
+import React, { useState, useEffect } from "react";
+import HabitCard from "../components/HabitCard/HabitCard";
+
 const Homepage = () => {
-  return <>Home Page</>;
+  const [habits, setHabits] = useState([]);
+
+  /* TEMPORARY: Move to separate with other requests when implementing state management */
+  const API_URL = process.env.REACT_APP_API_URL;
+  const TOKEN = process.env.REACT_APP_TOKEN;
+
+  const getAllHabits = async () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+    const response = await fetch(API_URL + "/habit", config);
+    const data = await response.json();
+    setHabits(data);
+    console.log(data);
+  };
+  ////////////////////////////////////
+
+  useEffect(() => {
+    getAllHabits();
+  }, [habits]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "10px",
+      }}
+    >
+      {habits.map((habit) => (
+        <HabitCard key={habit._id} habit={habit} />
+      ))}
+    </div>
+  );
 };
 
 export default Homepage;
--- server/src/routes/habitRoutes.js
+++ server/src/routes/habitRoutes.js
@@ -3,12 +3,14 @@ import {
   createHabit,
   updateHabit,
   deleteHabit,
+  getAll,
   getHabit,
 } from "../controller/habitController.js";
 
 const habitRouter = Router();
 
 habitRouter.post("/", createHabit);
+habitRouter.get("/", getAll);
 habitRouter.get("/:id", getHabit);
 habitRouter.patch("/:id", updateHabit);
 habitRouter.delete("/:id", deleteHabit);
