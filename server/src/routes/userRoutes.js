fc7df069e9f1e10dc3c0c0ef3bfa13cd40e9d64e
Rob Lehmann <lehmannrobb@gmail.com>
2023-03-02 13:39:55 +0300

    Added methods to get and update a user to the user controller and routes for these methods

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { userLogin, userSignUp } from "../controller/userController.js";
+import verifyToken from "../middleware/auth.js";
+import {
+  userLogin,
+  userSignUp,
+  getMe,
+  updateMe,
+} from "../controller/userController.js";
 const userRouter = Router();
 
 userRouter.post("/login", userLogin);
 userRouter.post("/signup", userSignUp);
+userRouter.get("/me", verifyToken, getMe);
+userRouter.patch("/me", verifyToken, updateMe);
 
 export default userRouter;
