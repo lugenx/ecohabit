c09d884324f394022d7b1e6210ec8a78cfcb107e
codepley <officialkushalkaran@gmail.com>
2023-03-18 18:58:09 +0530

    added error handling in getAnswer, updateAnswer, and deleteAnswer functions in answerController.js

--- server/src/controller/answerController.js
+++ server/src/controller/answerController.js
@@ -18,49 +18,61 @@ const createAnswer = async (req, res) => {
 const getAnswer = async (req, res) => {
   const id = req.params.id;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "Bad Request" });
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Bad Request" });
+    }
+
+    const answer = await Answer.findById(id);
+
+    if (!answer) {
+      return res.status(404).json({ error: "answer not found" });
+    }
+
+    res.status(200).json(answer);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
-
-  const answer = await Answer.findById(id);
-
-  if (!answer) {
-    return res.status(404).json({ error: "answer not found" });
-  }
-
-  res.status(200).json(answer);
 };
 
 const updateAnswer = async (req, res) => {
   const id = req.params.id;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "Bad Request" });
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Bad Request" });
+    }
+
+    const answer = await Answer.findOneAndUpdate({ _id: id }, { ...req.body });
+
+    if (!answer) {
+      return res.status(404).json({ error: "answer not found" });
+    }
+
+    res.status(201).json(answer);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
-
-  const answer = await Answer.findOneAndUpdate({ _id: id }, { ...req.body });
-
-  if (!answer) {
-    return res.status(404).json({ error: "answer not found" });
-  }
-
-  res.status(201).json(answer);
 };
 
 const deleteAnswer = async (req, res) => {
   const id = req.params.id;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "Bad Request" });
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Bad Request" });
+    }
+
+    const answer = await Answer.findOneAndDelete({ _id: id });
+
+    if (!answer) {
+      return res.status(404).json({ error: "answer not found" });
+    }
+
+    res.status(200).json(answer);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
-
-  const answer = await Answer.findOneAndDelete({ _id: id });
-
-  if (!answer) {
-    return res.status(404).json({ error: "answer not found" });
-  }
-
-  res.status(200).json(answer);
 };
 
 export { createAnswer, getAnswer, updateAnswer, deleteAnswer };
