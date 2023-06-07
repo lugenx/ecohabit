84cfc3adfff83f61914be4f7bb56fab584d60b52
Elgun Asgar <elgunasgerov@gmail.com>
2023-06-07 18:47:15 -0400

    Enhance User Context to Include User Data and Error Handling

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -8,15 +8,34 @@ const UserContextProvider = (props) => {
     const tokenValue = localStorage.getItem("token");
     return tokenValue;
   });
-
+  const [error, setError] = useState(null);
   useEffect(() => {
-    if(token){
-    console.log("fetching user data");
+    if (token) {
+      const fetchUserData = async () => {
+        try {
+          const response = await fetch(
+            `${process.env.REACT_APP_API_URL}/user/me`,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          const userData = await response.json();
+          setUser(userData);
+        } catch (error) {
+          console.log("Error while fetching user data", error);
+          setError(error.message);
+        }
+      };
+      fetchUserData();
+      // console.log("fetching user data");
     }
   }, [token]);
 
   return (
-    <UserContext.Provider value={{ user, setUser, token, setToken }}>
+    <UserContext.Provider value={{ user, setUser, token, setToken, error }}>
       {props.children}
     </UserContext.Provider>
   );
