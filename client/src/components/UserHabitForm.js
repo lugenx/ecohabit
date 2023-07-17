ea09ccd579a23ff21d90c2cffe6fdc3144d677b6
Rob Lehmann <lehmannrobb@gmail.com>
2023-07-17 16:25:46 +0300

    Added category images to HabitCard component

--- client/src/components/UserHabitForm.js
+++ client/src/components/UserHabitForm.js
@@ -1,74 +1,95 @@
 import React from "react";
 import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 
-const UserHabitForm = ({ toggleForm, onAdd, categories, habits, myHabits }) => {
+const UserHabitForm = ({
+  toggleForm,
+  onAdd,
+  onRemove,
+  categories,
+  habits,
+  myHabits,
+}) => {
   // Checks if habit exists in myHabits array
   const inMyHabits = (id) => {
     return myHabits.some((item) => item._id === id);
   };
+
+  // Determine to add or remove
+  const habitHandler = (habit) => {
+    if (inMyHabits(habit._id)) {
+      onRemove(habit._id);
+    } else {
+      onAdd(habit);
+    }
+  };
+
+  // Ensures modal stays open when inside the form clicked
+  const handleFormClick = (event) => {
+    // Prevent the event from propagating to the outer Box
+    event.stopPropagation();
+  };
+
   return (
     <Box>
-      <Card sx={{ padding: "1.5rem" }}>
-        <CardContent>
-          {categories.map((category, index) => (
-            <Box key={index}>
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                color="#05c46b"
-              >
-                {category}
-              </Typography>
-              {habits.map((habit) => {
-                if (habit.category === category) {
-                  return (
-                    <Box
-                      sx={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                      }}
-                      key={habit._id}
-                    >
-                      <Typography variant="body1" color="text.secondary">
-                        {habit.description}
-                      </Typography>
-                      <Button
-                        onClick={() => onAdd(habit)}
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.5)", // Translucent overlay
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          zIndex: 9999, // Higher z-index to be above other content
+        }}
+        onClick={toggleForm}
+      >
+        <Card sx={{ padding: "0.5rem" }} onClick={handleFormClick}>
+          <CardContent>
+            {categories.map((category, index) => (
+              <Box key={index}>
+                <Typography variant="h5" component="div" color="#05c46b">
+                  {category}
+                </Typography>
+                {habits.map((habit) => {
+                  if (habit.category === category) {
+                    return (
+                      <Box
                         sx={{
-                          color: inMyHabits(habit._id)
-                            ? "text.secondary"
-                            : "#05c46b",
-                          cursor: "pointer",
-                          fontSize: "24px",
-                          disabled: inMyHabits(habit._id),
+                          display: "flex",
+                          justifyContent: "space-between",
+                          alignItems: "center",
                         }}
+                        key={habit._id}
                       >
-                        +
-                      </Button>
-                    </Box>
-                  );
-                } else {
-                  return null;
-                }
-              })}
-            </Box>
-          ))}
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              mt: 2,
-            }}
-          >
-            <Button size="medium" variant="outlined" onClick={toggleForm}>
-              Cancel
-            </Button>
-          </Box>
-        </CardContent>
-      </Card>
+                        <Typography variant="body1" color="text.secondary">
+                          {habit.description}
+                        </Typography>
+                        <Button
+                          onClick={() => habitHandler(habit)}
+                          sx={{
+                            color: inMyHabits(habit._id)
+                              ? "text.secondary"
+                              : "#05c46b",
+                            cursor: "pointer",
+                            fontSize: "24px",
+                          }}
+                        >
+                          {inMyHabits(habit._id) ? "-" : "+"}
+                        </Button>
+                      </Box>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
+              </Box>
+            ))}
+          </CardContent>
+        </Card>
+      </Box>
     </Box>
   );
 };
