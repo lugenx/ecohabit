2385db40fc7545600ccbd603d32f6a76c2ed4744
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2023-08-14 14:03:59 +0100

    update ResponsvieAppBar auth status check implementation

--- client/src/components/ResponsiveAppBar.js
+++ client/src/components/ResponsiveAppBar.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useLoginContext } from "../contexts/LoginContext.js";
 import { Adb as AdbIcon, Menu as MenuIcon } from "@mui/icons-material";
 import {
   AppBar,
@@ -16,6 +15,8 @@ import {
   MenuItem,
 } from "@mui/material";
 
+import { useUserContext } from "../contexts/UserContext.js";
+
 const pages = ["Home", "Locations", "Guides", "About"];
 
 function ResponsiveAppBar() {
@@ -27,15 +28,22 @@ function ResponsiveAppBar() {
     "Dashboard",
     "Login",
   ]);
-  const { loggedIn } = useLoginContext();
+  const { user } = useUserContext();
 
   useEffect(() => {
-    if (loggedIn) {
-      setSettings([...settings.slice(0, settings.length - 1), "Logout"]);
+    if (user) {
+      setSettings((prevSettings) => [
+        ...prevSettings.slice(0, prevSettings.length - 1),
+        "Logout",
+      ]);
     } else {
-      setSettings([...settings.slice(0, settings.length - 1), "Login"]);
+      setSettings((prevSettings) => [
+        ...prevSettings.slice(0, prevSettings.length - 1),
+        "Login",
+      ]);
     }
-  }, [loggedIn]);
+  }, [user]);
+
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
