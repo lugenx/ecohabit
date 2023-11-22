6ce914b4a585593b2c4909a684302b77388027e1
aegucer <43250844+emregucerr@users.noreply.github.com>
2023-11-22 00:40:19 -0600

    Refactor authentication logic

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import { UserContextProvider } from "./contexts/UserContext";
  *To avoid bloated App.js component, page routes created at Routes.js component.
  */
 
+ // This is a change made by a human - one of the last of its kinds
+
 function App() {
   return (
     <>
